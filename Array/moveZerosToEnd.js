3dcd0e200087af21e3f998a8006c97e8574135db
Muhammed Abdurahman C K <abdurahmanck715@gmail.com>
2026-04-22 07:48:07 +0530

    Solved move zeros to the end of the array problem

---
+++ Array/moveZerosToEnd.js
@@ -0,0 +1,19 @@
+//286-Move zeros to the end of the array.
+function moveZeros(nums) {
+  let x = 0;
+  let i;
+  for (i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      nums[x] = nums[i];
+      x++;
+    }
+  }
+  //Fill rest of the elements with zeros
+  for (let i = x; i < nums.length; i++) {
+    nums[i] = 0;
+  }
+  return nums;
+}
+let nums = [0, 1, 3, 0];
+let result = moveZeros(nums);
+console.log(result);
