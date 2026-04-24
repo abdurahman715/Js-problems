dc9fc9c906b53db7b8ef53dfb3aeaa3390f673cf
Muhammed Abdurahman C K <abdurahmanck715@gmail.com>
2026-04-24 07:56:31 +0530

    Solved single number problem approach-2

--- Array/singleNumber.js
+++ Array/singleNumber.js
@@ -1,20 +1,29 @@
 //136-Single number
 //Approach-1
-function singleNumber(nums) {
-  let hash = {};
+// function singleNumber(nums) {
+//   let hash = {};
+//   for (let i = 0; i < nums.length; i++) {
+//     if (!hash[nums[i]]) {
+//       hash[nums[i]] = 1;
+//     } else {
+//       hash[nums[i]]++;
+//     }
+//   }
+//   for (let i = 0; i < nums.length; i++) {
+//     if (hash[nums[i]] == 1) {
+//       return nums[i];
+//     }
+//   }
+// }
+
+//Approach-2
+function singleNumber2(nums) {
+  let xor = 0;
   for (let i = 0; i < nums.length; i++) {
-    if (!hash[nums[i]]) {
-      hash[nums[i]] = 1;
-    } else {
-      hash[nums[i]]++;
-    }
+    xor = xor ^ nums[i];
   }
-  for (let i = 0; i < nums.length; i++) {
-    if (hash[nums[i]] == 1) {
-      return nums[i];
-    }
-  }
+  return xor;
 }
 let nums = [1, 2, 3, 4, 5, 2, 3, 4, 5];
-let result = singleNumber(nums);
+let result = singleNumber2(nums);
 console.log(result);
