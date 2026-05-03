f5e5b6903db12f9121cc82de2f724ba74365f98a
Muhammed Abdurahman C K <abdurahmanck715@gmail.com>
2026-05-03 07:15:00 +0530

    Solved binary search problem

---
+++ Searching/binarySearch.js
@@ -0,0 +1,20 @@
+//704-Binary Search
+function binarySearch(nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+  while (right >= left) {
+    let middle = Math.floor((left + right) / 2);
+    if (target === nums[middle]) {
+      return middle;
+    } else if (target < nums[middle]) {
+      right = middle - 1;
+    } else {
+      left = middle + 1;
+    }
+  }
+  return -1;
+}
+let nums = [-1, 0, 3, 5, 9, 12];
+let target = 9;
+let result = binarySearch(nums, target);
+console.log(result);
