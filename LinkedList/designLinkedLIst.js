cc9fa2ee38dd12578af9c7a3271ba3c8653eaccd
Muhammed Abdurahman C K <abdurahmanck715@gmail.com>
2026-05-08 08:03:57 +0530

    Solved design linked list problem

---
+++ LinkedList/designLinkedLIst.js
@@ -0,0 +1,81 @@
+//707-Desing Linked List
+function Node(val) {
+  this.val = val;
+  this.next = null;
+}
+
+let MyLinkedList = function () {
+  this.head = null;
+  this.size = 0;
+  console.log(MyLinkedList);
+};
+
+MyLinkedList.prototype.get = function (index) {
+  if (index < 0 || index >= this.size) {
+    return -1;
+  }
+  let curr = this.head;
+  for (let i = 0; i < index; i++) {
+    curr = curr.next;
+  }
+  return curr.val;
+};
+
+MyLinkedList.prototype.addAtHead = function (val) {
+  let newNode = new Node(val);
+  newNode.next = this.head;
+  this.head = newNode;
+  this.size++;
+};
+
+MyLinkedList.prototype.addAtTail = function (val) {
+  let newNode = new Node(val);
+  if (this.head == null) {
+    this.head = newNode;
+  } else {
+    let curr = this.head;
+    while (curr.next != null) {
+      curr = curr.next;
+    }
+    curr.next = newNode;
+  }
+  this.size++;
+};
+
+MyLinkedList.prototype.addAtIndex = function (index, val) {
+  let newNode = new Node(val);
+  if (index < 0 || index > this.size) {
+    return;
+  }
+  if (index === 0) {
+    this.addAtHead(val);
+    return;
+  } else if (index == this.size) {
+    this.addAtTail(val);
+    return;
+  } else {
+    let curr = this.head;
+    for (let i = 0; i < index - 1; i++) {
+      curr = curr.next;
+    }
+    newNode.next = curr.next;
+    curr.next = newNode;
+  }
+  this.size++;
+};
+
+MyLinkedList.prototype.deleteAtIndex = function (index) {
+  if (index < 0 || index >= this.size) {
+    return;
+  }
+  if (index == 0) {
+    this.head = this.head.next;
+  } else {
+    let curr = this.head;
+    for (let i = 0; i < index - 1; i++) {
+      curr = curr.next;
+    }
+    curr.next = curr.next.next;
+  }
+  this.size--;
+};
