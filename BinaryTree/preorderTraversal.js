d69120f21ce114ee5a51753a7522778e670576ef
Muhammed Abdurahman C K <abdurahmanck715@gmail.com>
2026-06-15 08:36:12 +0530

    Pre order traversal problem

---
+++ BinaryTree/preorderTraversal.js
@@ -0,0 +1,12 @@
+//144-Binary Tree PreOrder Traversal
+var preorderTraversal = function (root) {
+  let ans = [];
+  function traversal(curr) {
+    if (!curr) return;
+    ans.push(curr?.val);
+    traversal(curr?.left);
+    traversal(curr?.right);
+  }
+  traversal(root);
+  return ans;
+};
